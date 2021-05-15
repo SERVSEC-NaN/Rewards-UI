85bacb494a593dbb472e799f0b9b30aba66fc18c
Bijon Setyawan Raya <nathansetyawan96>
2021-05-15 16:23:50 +0800

    question and options font size resized

--- pages/choose.tsx
+++ pages/choose.tsx
@@ -4,15 +4,15 @@ import styles from '../styles/choose.module.css'
 const Choose: React.FunctionComponent = (): JSX.Element => {
   return (
     <>
-      <h1 className="text-6xl mb-10">Are you a?</h1>
+      <h1 className="text-5xl mb-10">Are you a?</h1>
       <button
-        className={styles.button + " py-2 px-4 mb-5 text-5xl border border-gray-400 shadow"}
+        className={styles.button + " py-2 px-4 mb-5 text-3xl border border-gray-400 shadow"}
         style={{
           width: `60%`
         }}
       >Subscriber</button>
       <button
-        className={styles.button + " py-2 px-4 mb-5 text-5xl border border-gray-400 shadow"}
+        className={styles.button + " py-2 px-4 mb-5 text-3xl border border-gray-400 shadow"}
         style={{
           width: `60%`
         }}
