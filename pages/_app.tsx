82d43db3c3147fef986b784c104c8c60b70e2cf5
Bijon Setyawan Raya <nathansetyawan96>
2021-05-15 16:05:37 +0800

    dark mode removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
-
 import { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
-
 import '../styles/global.css'
 
 const Rewards: React.FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
