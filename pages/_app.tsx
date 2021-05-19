7ff19050145048328d1b9a5720bbfb7578e2c084
nathansetyawan96 <nathan.setyawan96@gmail.com>
2021-05-19 23:36:07 +0800

    problem: reserved word this in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 import '../styles/global.css'
 
-const Rewards: React.FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps): JSX.Element => {
+const MyApp: React.FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <link
+          rel="stylesheet"
+          href={`${this.props.__NEXT_DATA__.assetPrefix}/_next/static/style.css`}
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
-export default Rewards
+export default MyApp
